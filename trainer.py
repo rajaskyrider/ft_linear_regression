27d4e4358255090877638ba8a12b607f716a922e
Raja Pandiperumal <raja.pandiperumal@proton.me>
2026-04-27 09:56:12 +0200

    10%

--- trainer.py
+++ trainer.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 
 class regression:
-	def __init__(self, theta0:int, theta1:int, learning_rate:int = 0.01, threshold = 0.0001, iter = 1000, path="./model_params.csv"):
+	def __init__(self, theta0:int, theta1:int, learning_rate:float = 0.1, threshold:float = 0.0001, iter:int = 1000, path:str ="./model_params.csv"):
 		self.theta0 = theta0
 		self.theta1 = theta1
 		self.learning_rate = learning_rate
@@ -18,15 +18,24 @@ class regression:
 
 	def train(self, mileage:pd.Series, price:pd.Series):
 		m = mileage.size
+		mean_km = mileage.mean()
+		std_km = mileage.std()
+		mileage_scaled  = (mileage - mean_km) / std_km
 		for iteration in range(self.iter):
-			error = self.theta0 + (self.theta1 * mileage) - price
+		#while (True):
+			error = self.theta0 + (self.theta1 * mileage_scaled) - price
+			#print("error : ", error.sum())
 			tmp0 = self.learning_rate * error.sum() / m
-			tmp1 = self.learning_rate * (error * mileage).sum() / m
+			#print("tmp0 : ", tmp0)
+			tmp1 = self.learning_rate * (error * mileage_scaled).sum() / m
+			#print("tmp1 : ", tmp1)
 			self.theta0 -= tmp0
 			self.theta1 -= tmp1
 			if (abs(tmp0) < self.eps and abs(tmp1) < self.eps):
 				break
-		save_model()
+		self.theta1 = self.theta1 / std_km
+		self.theta0 = self.theta0 - self.theta1 * mean_km
+		self.save_model()
 
 
 def main():
@@ -40,7 +49,7 @@ def main():
 	#plt.title("Price vs Mileage")
 	#plt.gca().xaxis.set_major_formatter(mtick.FuncFormatter(lambda x, _: f"{int(x / 1000)}k"))
 	#plt.show()
-	reg = regression(1, 1)
+	reg = regression(0,0)
 	reg.train(km, price)
 
 
